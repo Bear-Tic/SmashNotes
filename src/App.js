55934919ef46e9e46c6a4f34f4e1ea0f162b08e9
Philippe Chevreul <tic@bearstudio.fr>
2020-02-06 11:50:31 +0100

    update readme and customTheme with material ui

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import { SignUp } from './ui/pages/SignUp';
 import { Header } from './ui/components/Header';
 import { Nav } from './ui/components/Nav';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, Button } from '@material-ui/core';
+import theme from './config/theme/theme';
 
 const App = () => {
   return (
-    <div>
-      <CssBaseline />
-      <Header />
-      <SignUp />
-      <Nav />
-    </div>
+    <>
+      <MuiThemeProvider theme={theme}>
+          <CssBaseline />
+          <Header />
+          <SignUp />
+          <Nav />
+          <Button variant="contained" color="primary">lalala</Button>
+      </MuiThemeProvider>
+    </>
   );
 };
 
---
+++ src/config/theme/theme.js
@@ -0,0 +1,4 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import theme from './customThemes';
+
+export default createMuiTheme(theme);
