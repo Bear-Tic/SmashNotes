77818a57ac1084d512bbdf68b72578c3f61161fe
Philippe Chevreul <tic@bearstudio.fr>
2020-02-07 00:45:34 +0100

    feat: comment call

--- src/ui/components/Nav/index.js
+++ src/ui/components/Nav/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from "prop-types";
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 import BookTwoToneIcon from '@material-ui/icons/BookTwoTone';
@@ -7,8 +7,28 @@ import HourglassFullTwoToneIcon from '@material-ui/icons/HourglassFullTwoTone';
 import PollTwoToneIcon from '@material-ui/icons/PollTwoTone';
 import { withRouter } from 'react-router-dom';
 
-const Nav = ({ history }) => {
-  const [value, setValue] = React.useState(0);
+const Nav = ({ history, location }) => {
+  const [value, setValue] = React.useState();
+  const myMains = "/mymains";
+  const matchUp = "/matchup"
+  const matchHistory = "/matchhistory";
+  const glossary = "/glossary";
+
+  useEffect(() => {
+    if(location.pathname === myMains) {
+      setValue(0);
+    }
+    if(location.pathname === matchUp) {
+      setValue(1);
+    }
+    if(location.pathname === matchHistory) {
+      setValue(2);
+    }
+    if(location.pathname === glossary) {
+      setValue(3);
+    }
+  }, [location]);
+
   return (
     <div className="p-body__navigation">
       <BottomNavigation
@@ -16,12 +36,11 @@ const Nav = ({ history }) => {
         onChange={(event, newValue) => {
           setValue(newValue);
         }}
-        showLabels
       >
-        <BottomNavigationAction icon={<BookTwoToneIcon />} onClick={() => history.push('/mymains')} />
-        <BottomNavigationAction icon={<PollTwoToneIcon />} onClick={() => history.push('/matchup')} />
-        <BottomNavigationAction icon={<HourglassFullTwoToneIcon onClick={() => history.push('/matchhistory')} />} />
-        <BottomNavigationAction icon={<ImportContactsTwoToneIcon onClick={() => history.push('/glossary')} />} />
+        <BottomNavigationAction icon={<BookTwoToneIcon />} onClick={() => history.push(myMains)} />
+        <BottomNavigationAction icon={<PollTwoToneIcon />} onClick={() => history.push(matchUp)} />
+        <BottomNavigationAction icon={<HourglassFullTwoToneIcon onClick={() => history.push(matchHistory)} />} />
+        <BottomNavigationAction icon={<ImportContactsTwoToneIcon onClick={() => history.push(glossary)} />} />
       </BottomNavigation>
     </div>
   );
--- src/ui/pages/MyMains/index.js
+++ src/ui/pages/MyMains/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAllCharacters } from '../../../api/api';
+import { Card } from '@material-ui/core';
 
 export const MyMains = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [characters, setCharacters] = useState();
+
+  // useEffect( () => {
+  //   const fetchData = async () => {
+  //     setCharacters(await getAllCharacters());
+  //     setIsLoading(false);
+  //   }
+  //   fetchData();
+  // }, [setCharacters]);
+
   return (
     <>
-      MyMains Component
+      {isLoading ?
+        'Loading...'
+          :
+          (
+            characters.map((character, index) => <Card key={index}>{character.data.name}</Card>)
+          )
+      }
     </>
   );
 };
