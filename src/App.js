3721ed9bf77cbac2f32d5c75ac40b1f139764844
Philippe Chevreul <tic@bearstudio.fr>
2020-07-05 20:42:41 +0200

    refacto time

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
 import React from 'react';
-import "./scss/index.scss";
 import { Body } from './ui/components/Body';
 import { Header } from './ui/components/Header';
 import Nav from './ui/components/Nav';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { MuiThemeProvider, Container } from '@material-ui/core';
-import theme from './config/theme/theme';
 import { BrowserRouter as Router } from 'react-router-dom';
 import withFirebaseAuth from 'react-with-firebase-auth'
 import * as firebase from 'firebase';
 import 'firebase/auth';
 import firebaseConfig from './firebase/firebase';
-
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firebaseAppAuth = firebaseApp.auth();
 const providers = {
   googleProvider: new firebase.auth.GoogleAuthProvider(),
 };
 
-const App = ({ user, signOut, signInWithGoogle }) => {
+const App = ({ user, signOut, signInWithGoogle }) => {
   return (
     <>
-      <MuiThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="p-body">
-          <Router>
-            <Header user={user} signOut={signOut} signInWithGoogle={signInWithGoogle} />
-            <div className="p-body__main">
-              <Container>
-                <Body user={user} />
-              </Container>
-            </div>
-            <Nav />
-          </Router>
-        </div>
-      </MuiThemeProvider>
+      <div className="p-body">
+        <Router>
+          <Header user={user} signOut={signOut} signInWithGoogle={signInWithGoogle} />
+          <div>
+            <Body user={user} />
+          </div>
+          <Nav />
+        </Router>
+      </div>
     </>
   );
 };
--- src/ui/pages/UnknownUrl/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const UnknownUrl = () => {
-  return (
-    <>
-      Error 404.
-    </>
-  );
-};
