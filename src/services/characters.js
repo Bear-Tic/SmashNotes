7a729ee6116c4c969c15ba03fa40a3c4650a7f90
Philippe Chevreul <tic@bearstudio.fr>
2020-07-15 00:40:40 +0200

    stale time

--- src/services/characters.js
+++ src/services/characters.js
@@ -2,4 +2,6 @@ import axios from 'axios';
 import { useQuery } from 'react-query';
 
 export const useCharacters = (params) => useQuery(['characters', params],
-  async () => axios.get('https://us-central1-smashnotes-b07e9.cloudfunctions.net/app/api/characters', { params }));
+  async () => axios.get('https://us-central1-smashnotes-b07e9.cloudfunctions.net/app/api/characters', { params }), {
+    staleTime: 1 * 1000 * 60 * 60 * 3,
+  });
